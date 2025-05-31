54d738f33ce5f94faf1fdc2293e0bfb6cf8c1ad0
zmilla93 <zmilla93@gmail.com>
2025-05-31 18:19:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://zmilla93.github.io",
-  base: "astro-lab",
+  // site: "https://zmilla93.github.io",
+  base: "/astro-lab/",
 });
